8467f946869289ecd271fba3354641b5b9908cae
milkomardev <122818859+milkomardev@users.noreply.github.com>
2024-06-14 17:57:07 +0300

    Added more solutions

---
+++ PostgreSQL - Exercises/2. Basic CRUD - Exercise/19_create_a_view.sql
@@ -0,0 +1,8 @@
+CREATE VIEW view_company_chart
+AS SELECT
+		  "Full Name",
+		  "Job Title"
+FROM company_chart
+WHERE "Manager ID" = 184;
+
+SELECT * FROM view_company_chart;
---
+++ PostgreSQL - Exercises/2. Basic CRUD - Exercise/24_substring.sql
@@ -0,0 +1,19 @@
+CREATE VIEW view_initials
+AS SELECT
+		  SUBSTRING(first_name FROM 1 FOR 2) AS "initial",
+		  last_name
+FROM employees
+ORDER BY last_name;
+
+
+-- second solution:
+
+-- CREATE VIEW view_initials 
+-- AS SELECT
+--      first_name AS "initial", 
+--      last_name
+-- FROM employees
+-- ORDER BY last_name;
+--
+-- UPDATE view_initials
+-- SET initial = LEFT(initial, 2);
