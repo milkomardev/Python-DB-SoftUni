1d1e112bfd29dc3dd2f01bd68afc0618cbe9d251
milkomardev <122818859+milkomardev@users.noreply.github.com>
2024-06-28 20:01:25 +0300

    Added more solutions

---
+++ PostgreSQL - Exercises/4. Data Aggregation - Exercise/19_foreign_key.sql
@@ -0,0 +1,28 @@
+CREATE TABLE employees_projects(
+	id SERIAL PRIMARY KEY,
+	employee_id INTEGER,
+	project_id INTEGER,
+
+	FOREIGN KEY(employee_id) REFERENCES employees(id),
+	FOREIGN KEY(project_id) REFERENCES projects(id)
+);
+
+
+
+
+
+
+-- second solution
+
+-- CREATE TABLE employees_projects(
+-- 	id SERIAL PRIMARY KEY,
+-- 	employee_id INTEGER,
+-- 	project_id INTEGER,
+--
+-- 	CONSTRAINT fk_employee_id
+-- 		FOREIGN KEY (employee_id)
+-- 		REFERENCES employees(id),
+-- 	CONSTRAINT fk_project_id
+-- 		FOREIGN KEY(project_id)
+-- 		REFERENCES projects(id)
+-- );
