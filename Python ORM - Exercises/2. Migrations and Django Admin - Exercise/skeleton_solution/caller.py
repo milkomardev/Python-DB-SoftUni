8eec130f0042a4e6132d0ffa7b5bd4c08e7dd821
milkomardev <122818859+milkomardev@users.noreply.github.com>
2024-08-19 16:29:14 +0300

    Added more solutions

---
+++ Python ORM - Exercises/2. Migrations and Django Admin - Exercise/skeleton_solution/caller.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+# Create queries within functions
+
+from main_app.models import Shoe
+
+print(Shoe.objects.values_list('brand', flat=True).distinct())
